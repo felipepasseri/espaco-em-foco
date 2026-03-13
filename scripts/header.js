9ef18e8077b8a1104010f122247e3d037db37aa6
felipepasseri <reisfelipe429@gmail.com>
2026-03-12 21:25:37 -0300

    Corrige erros de funcionalidade do botao do header

--- scripts/header.js
+++ scripts/header.js
@@ -3,9 +3,24 @@ export default function abrirMenu() {
     const menuPrincipal = document.getElementById('main-nav-container')
     const botaoLogin = document.getElementById('login-container')
     botaoMenu.addEventListener('click', () => {
-        if (botaoMenu.checked) {
-            menuPrincipal.style.display = 'block'
-            botaoLogin.style.display = 'block'
+        if (window.innerWidth <= 425) {
+            if (botaoMenu.checked) {
+                menuPrincipal.style.display = 'block'
+                botaoLogin.style.display = 'block'
+            }
+            else {
+                menuPrincipal.style.display = 'none'
+                botaoLogin.style.display = 'none'
+            }
+        }
+        else {
+            return
+        }
+    })
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 425) {
+            menuPrincipal.style.display = 'flex'
+            botaoLogin.style.display = 'flex'
         }
         else {
             menuPrincipal.style.display = 'none'
